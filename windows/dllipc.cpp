6b63531d4326a030f72ee26287956c9a25ef7602
andor <andor@fdc763c4-2c80-ea4c-99bf-9e619a75be50>
2011-12-28 09:56:19 +0000

    исправил IPC_openLibraryEx для win64

--- windows/dllipc.cpp
+++ windows/dllipc.cpp
@@ -11,28 +11,29 @@
 #ifdef _WIN64
 IPC_handle IPC_openLibraryEx(const IPC_str *baseName, unsigned param)
 {
-    ipc_handle_t h = allocate_ipc_object(name, IPC_typeLibrary);
+    ipc_handle_t h = allocate_ipc_object(baseName, IPC_typeLibrary);
     if(!h)
         return NULL;
 
 	IPC_str libname[MAX_PATH];
 	IPC_str libname0[MAX_PATH];
 	wcscpy(libname, baseName);
-	wchar_t* pchar = wcsstr(libname, _BRDC(".dll"));
+	wchar_t* pchar = wcsstr(libname, L".dll");
 	if(pchar != NULL)
 		*pchar = 0;
 	size_t len_name =  wcslen(libname);
 	wcscpy(libname0, libname);
 	if(libname[len_name-2] != '6' && libname[len_name-1] != '4')
 	{
-		wcscat(libname, _BRDC("64"));
-		wcscat(libname0, _BRDC("-64"));
+		wcscat(libname, L"64");
+		wcscat(libname0, L"-64");
 	}
-            h->ipc_descr = IPC_openLibrary( libname, 0 );
+            h->ipc_descr = LoadLibrary(libname);
             if( !h->ipc_descr )
 	{
-                    h->ipc_descr = IPC_openLibrary( libname0, 0 );
+                    h->ipc_descr = LoadLibrary(libname0);
 	}
+    return h;
 }
 #else
 IPC_handle IPC_openLibraryEx(const IPC_str *baseName, unsigned param)
