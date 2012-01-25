e78f9e2c9956a0dab7b662cf0e9cfa65cf8049ad
Karakozov <Karakozov@fdc763c4-2c80-ea4c-99bf-9e619a75be50>
2012-01-25 12:00:21 +0000

    Косметические улучшения.

--- include/semipc.h
+++ include/semipc.h
@@ -10,37 +10,40 @@
 // Функции для работы с семаформаи
 //----------------------------------------------------------------------
 
+extern "C" {
+
     //! Функция создает именованный семафор
     /*!
     \param name - имя семафора
     \param value - начальное значение семафора
     */
-    extern "C" IPC_handle IPC_createSemaphore(const IPC_str *name, int value);
+    IPC_handle IPC_createSemaphore(const IPC_str *name, int value);
 
     //! Функция создает именованный семафор с дополнительным параметром
     /*!
     \param name - имя семафора
     \param value - начальное значение семафора
     */
-    extern "C" IPC_handle IPC_createSemaphoreEx(const IPC_str *name, int value, ... );
+    IPC_handle IPC_createSemaphoreEx(const IPC_str *name, int value, ... );
 
     //! Функция захватывает семафор или ждет его освобождения
     /*!
     \param handle - дескриптор семафора
     \param timeout - время ожидания операции
     */
-    extern "C" int IPC_lockSemaphore(const IPC_handle handle, int timeout);
+    int IPC_lockSemaphore(const IPC_handle handle, int timeout);
 
     //! Функция освобождает занятый семафор
     /*!
     \param handle - дескриптор семафора
     */
-    extern "C" int IPC_unlockSemaphore(const IPC_handle handle);
+    int IPC_unlockSemaphore(const IPC_handle handle);
 
     //! Функция удаляет именованный объект семафора
     /*!
     \param handle - дескриптор семафора
     */
-    extern "C" int IPC_deleteSemaphore(IPC_handle handle);
+    int IPC_deleteSemaphore(IPC_handle handle);
+}
 
 #endif //__SEMIPC_H__
