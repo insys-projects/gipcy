6f9ea5bf03d8eb82564a4e6d4d09178e7512ae3e
Karakozov <Karakozov@fdc763c4-2c80-ea4c-99bf-9e619a75be50>
2012-01-27 09:08:30 +0000

    Добавлено описание функций для гененрирования документации по функциям.

--- include/dllipc.h
+++ include/dllipc.h
@@ -12,11 +12,35 @@
 
 extern "C" {
 
-IPC_handle IPC_openLibraryEx(const IPC_str *baseName, unsigned param);
-IPC_handle IPC_openLibrary(const IPC_str *name, unsigned param);
-void* IPC_getEntry(IPC_handle handle, const char *entryName);
-int IPC_closeLibrary(IPC_handle handle);
-char* IPC_libraryError(void);
+    //! Функция открывает разделяемую библиотеку c дополнительными параметрами
+    /*!
+    \param name - имя библиотеки
+    \param param - дополнительный параметр
+    */
+    IPC_handle IPC_openLibraryEx(const IPC_str *baseName, unsigned param);
+
+    //! Функция открывает разделяемую библиотеку (динамическую библиотеку)
+    /*!
+    \param name - имя библиотеки
+    \param param - дополнительный параметр
+    */
+    IPC_handle IPC_openLibrary(const IPC_str *name, unsigned param);
+
+    //! Функция возвращает адрес точки входа в функцию находящуюся в разделяемой библиотеке
+    /*!
+    \param handle - дескриптор разделяемой библиотеки
+    \param entryName - имя точки входа
+    */
+    void* IPC_getEntry(IPC_handle handle, const char *entryName);
+
+    //! Функция закрывает разделяемую библиотеку
+    /*!
+    \param handle - дескриптор разделяемой библиотеки
+    */
+    int IPC_closeLibrary(IPC_handle handle);
+
+    //! Функция возвращает описание последней ошибки возникшей в разделяемой библиотеке
+    char* IPC_libraryError(void);
 
 };
 
