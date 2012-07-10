a1ce8fd6626a5cd7dc2b2d29762f419d2cfc59d2
Karakozov <Karakozov@fdc763c4-2c80-ea4c-99bf-9e619a75be50>
2012-07-10 12:45:29 +0000

    Добавлен тест мьютексов.

---
+++ libtest/mutex_simple_test.cpp
@@ -0,0 +1,70 @@
+
+#include "gipcy.h"
+
+#include <iostream>
+#include <string>
+#include <istream>
+#include <fstream>
+#include <iomanip>
+#include <cstring>
+#include <signal.h>
+
+using namespace std;
+
+//------------------------------------------------------------------
+
+void show_error(int err)
+{
+    switch(err) {
+    case IPC_OK:
+        std::cout << "IPC_OK" << endl;
+    break;
+    case IPC_WAIT_TIMEOUT:
+        std::cout << "IPC_WAIT_TIMEOUT" << endl;
+    break;
+    case IPC_GENERAL_ERROR:
+        std::cout << "IPC_GENERAL_ERROR" << endl;
+    break;
+    }
+}
+
+//------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Mutex test start" << endl;
+    
+    std::cout << "Create synchronize mutex..." << endl;
+    IPC_handle lockMutex = IPC_createMutex("lockMutex", 0);
+    if(!lockMutex) {
+        std::cout << "IPC_createMutex() failed" << endl;
+        return -1;
+    }
+
+    std::cout << "Ok" << endl;
+
+    int res = IPC_captureMutex(lockMutex, -1);
+    if(res != IPC_OK) {
+        std::cout << "IPC_captureMutex() failed" << endl;
+        show_error(res);
+    }
+    
+    std::cout << "Mutex locked..." << endl;
+    getchar();
+
+    res = IPC_releaseMutex(lockMutex);
+    if(res != IPC_OK) {
+        std::cout << "IPC_releaseMutex() failed" << endl;
+        show_error(res);
+    }
+    
+    std::cout << "Mutex unlocked..." << endl;
+    getchar();
+
+    std::cout << "Delete mutex..." << endl;
+    IPC_deleteMutex(lockMutex);
+
+    std::cout << "Ok" << endl;
+
+    return 0;
+}
