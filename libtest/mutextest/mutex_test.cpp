f064aaa124f3c8807a8cb0566c9e8f25c35775e4
Karakozov <Karakozov@fdc763c4-2c80-ea4c-99bf-9e619a75be50>
2012-12-06 15:06:01 +0000

    Форматирование кода и создание "захваченного"  мьютекса

--- libtest/mutextest/mutex_test.cpp
+++ libtest/mutextest/mutex_test.cpp
@@ -20,53 +20,48 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    IPC_handle lockMutex = IPC_createMutex("lockMutex", 0);
+    IPC_handle lockMutex = IPC_createMutex("lockMutex", true);
     if(!lockMutex) {
         std::cout << "IPC_createMutex() - Error" << endl;
         return -1;
     } else {
-
-    std::cout << "IPC_createMutex(0) - Ok" << endl;
-}
+        std::cout << "IPC_createMutex(0) - Ok" << endl;
+    }
 
     int res = IPC_captureMutex(lockMutex, 1000);
     if(res != IPC_OK) {
         std::cout << "IPC_captuteMutex(1000) - Error" << endl;
     } else {
-
-    std::cout << "IPC_captureMutex(1000) - Ok" << endl;
- }
+        std::cout << "IPC_captureMutex(1000) - Ok" << endl;
+    }
 
     res = IPC_releaseMutex(lockMutex);
     if(res != IPC_OK) {
         std::cout << "IPC_releaseMutex() - Error" << endl;
     } else {
-
-    std::cout << "IPC_releaseMutex() - Ok" << endl;
-}
+        std::cout << "IPC_releaseMutex() - Ok" << endl;
+    }
 
     res = IPC_captureMutex(lockMutex, -1);
     if(res != IPC_OK) {
         std::cout << "IPC_captureMutex(-1) - Error" << endl;
     } else {
-    std::cout << "IPC_captureMutex(-1) - Ok" << endl;
-}
+        std::cout << "IPC_captureMutex(-1) - Ok" << endl;
+    }
 
     res = IPC_releaseMutex(lockMutex);
     if(res != IPC_OK) {
         std::cout << "IPC_releaseMutex() - Error" << endl;
     } else {
-
-    std::cout << "IPC_releaseMutex() - Ok" << endl;
-}
+        std::cout << "IPC_releaseMutex() - Ok" << endl;
+    }
 
     res = IPC_deleteMutex(lockMutex);
     if(res != IPC_OK) {
         std::cout << "IPC_deleteMutex() - Error" << endl;
     } else {
-
-    std::cout << "IPC_deleteMutex() - Ok" << endl;
-}
+        std::cout << "IPC_deleteMutex() - Ok" << endl;
+    }
 
     IPC_cleanup();
 
