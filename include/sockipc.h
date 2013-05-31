eb51d0c29477d6d5caa5ca8a6e2dde01ccec01c0
_mra <_mra@fdc763c4-2c80-ea4c-99bf-9e619a75be50>
2013-05-31 11:27:12 +0000

    IPC socket - UDP протокол

--- include/sockipc.h
+++ include/sockipc.h
@@ -13,7 +13,10 @@ extern "C" {
 typedef struct 
 {
         unsigned short port;
-        char    addr[14];
+		union {
+			char    data[14];
+			unsigned long ip;
+		} addr;
 } IPC_sockaddr;
 	
 enum IPC_proto {
@@ -24,15 +27,21 @@ enum IPC_proto {
 
 int	IPC_initSocket( );
 
+IPC_sockaddr IPC_resolve( IPC_str* addr );
+
 IPC_handle	IPC_openSocket( IPC_proto proto );
 
 int IPC_listen( IPC_handle s, IPC_sockaddr* ip, int backlog );
 int IPC_connect( IPC_handle s, IPC_sockaddr* ip );
+int IPC_bind( IPC_handle s, IPC_sockaddr* ip );
 
 int IPC_select( IPC_handle s, int timeout );
 
 int IPC_send( IPC_handle s, char *data, int size, int timeout );
 int IPC_recv( IPC_handle s, char *data, int size, int timeout  );
+
+int IPC_sendTo( IPC_handle s, IPC_sockaddr* ip,char *data, int size, int timeout );
+int IPC_recvFrom( IPC_handle s, IPC_sockaddr* ip,char *data, int size, int timeout  );
 
 int IPC_closeSocket( IPC_handle s );
 
--- windows/sockipc.cpp
+++ windows/sockipc.cpp
@@ -3,11 +3,12 @@
 #ifndef __WINIPC_H__
     #include "winipc.h"
 #endif
-#ifndef __SHMIPC_H__
-    #include "shmipc.h"
-#endif
+
 
 #include "sockipc.h"
+
+#include <string.h>
+#include <stdlib.h>     /* atoi */
 
 #include <winsock.h>
 
@@ -22,9 +23,49 @@ int	IPC_initSocket( )
 	return 0;
 }
 
+IPC_sockaddr IPC_resolve( IPC_str* addr )
+{
+	IPC_sockaddr a;
+	
+	int port = INADDR_ANY;
+	
+	//FIXME: small?
+	char buffer[256];
+	strcpy( buffer, addr );
+
+	char *pp = strstr( buffer, ":" );
+
+	if( pp )
+	{
+		port = atoi( pp+1 );
+		*pp=0;
+	}
+
+	unsigned long ip = inet_addr( buffer );
+
+	if( ip == INADDR_NONE )
+		ip = INADDR_ANY;
+
+	a.port = port;
+	a.addr.ip = ip;
+
+	return a;
+}
+
 SOCKET _IPC_udp()
 {
-	return 0;
+	SOCKET s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP );
+	
+	int uMode=1;
+	if(ioctlsocket(s, FIONBIO, (u_long*)&uMode) == SOCKET_ERROR)
+		return SOCKET_ERROR;
+	
+	// make it broadcast capable
+	int	i = 1;
+	if( setsockopt( s, SOL_SOCKET, SO_BROADCAST, (char *)&i, sizeof(i) ) == SOCKET_ERROR )
+		return SOCKET_ERROR;	
+
+	return s;
 }
 
 SOCKET _IPC_tcp()
@@ -66,6 +107,120 @@ IPC_handle	IPC_openSocket( IPC_proto proto )
     }
 
     return h;
+}
+
+int IPC_bind( IPC_handle s, IPC_sockaddr* ip )
+{
+	 ipc_handle_t h = (ipc_handle_t)s;
+	 
+	 {
+		sockaddr_in srcAddr;
+		srcAddr.sin_family = AF_INET;
+		srcAddr.sin_port = htons( ip->port );
+		srcAddr.sin_addr.S_un.S_addr = ( ip->addr.ip );
+
+		if(bind( (SOCKET)h->ipc_descr, (sockaddr*)&srcAddr, sizeof(srcAddr)) == SOCKET_ERROR)
+			return -1;
+	 }
+
+	 return IPC_OK;
+}
+
+int IPC_sendTo( IPC_handle s, IPC_sockaddr* ip,char *data, int size, int timeout )
+{
+	sockaddr_in srcAddr;
+	int size_sockaddr = sizeof(srcAddr);
+
+	
+
+	srcAddr.sin_family = AF_INET;
+	srcAddr.sin_port = htons( ip->port );
+	srcAddr.sin_addr.S_un.S_addr = ( ip->addr.ip );
+	
+
+	int cnt;
+	
+	struct fd_set WriteSet;
+    struct timeval tval={0, 100};
+	
+	ipc_handle_t h = (ipc_handle_t)s;
+	
+	char *pbuf = data;
+	char *pend = data + size;	
+
+	while( true )
+	{
+		FD_ZERO(&WriteSet);
+		FD_SET( (SOCKET)h->ipc_descr, &WriteSet);
+
+		int r = select(1, 0, &WriteSet, 0, &tval);
+		
+		if(r == 0)
+		{
+			continue;
+		}
+
+		cnt = sendto((SOCKET)h->ipc_descr, pbuf, size, 0,(struct sockaddr*)&srcAddr, size_sockaddr );
+
+		if(cnt == SOCKET_ERROR)
+		{
+			//printf("error send \n" );
+			int err = GetLastError();
+
+			continue;
+		}
+
+		break;
+	}
+
+	return size;
+}
+
+int IPC_recvFrom( IPC_handle s, IPC_sockaddr* ip,char *data, int size, int timeout  )
+{
+	sockaddr_in srcAddr;
+	int size_sockaddr = sizeof(srcAddr);
+	
+	int cnt;
+	
+	struct fd_set WriteSet;
+    struct timeval tval={0, 100};
+	
+	ipc_handle_t h = (ipc_handle_t)s;
+	
+	char *pbuf = data;
+	char *pend = data + size;	
+
+	while( true )
+	{
+		FD_ZERO(&WriteSet);
+		FD_SET( (SOCKET)h->ipc_descr, &WriteSet);
+
+		int r = select(1, 0, &WriteSet, 0, &tval);
+		
+		if(r == 0)
+		{
+			continue;
+		}
+
+		cnt = recvfrom((SOCKET)h->ipc_descr, pbuf, size, 0,(struct sockaddr*)&srcAddr, &size_sockaddr );
+
+		if(cnt == SOCKET_ERROR)
+		{
+			//printf("error send \n" );
+			continue;
+		}
+
+		break;
+	}
+
+	if( ip )
+	{
+		ip->port = ntohs( srcAddr.sin_port ); 
+		ip->addr.ip = ( srcAddr.sin_addr.S_un.S_addr ); 
+	}
+
+	return size;
 }
 
 int IPC_closeSocket( IPC_handle s )
